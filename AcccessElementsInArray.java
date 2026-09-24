7dbb229dfc2fbaf060a44a3087d5e9c80ea1275e
Sidharth Kumar <sidharthkumar6440@gmail.com>
2026-09-24 15:37:07 +0530

    ARRAYS

---
+++ AcccessElementsInArray.java
@@ -0,0 +1,10 @@
+public class AcccessElementsInArray {
+    public static void main(String[] args){
+        int brr[]={1,2,3};
+        int n=brr.length;
+        for(int index=0;index<=n-1;index++){
+           System.out.println(brr[index]);
+        }
+    }
+    
+}
